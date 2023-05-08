d3dc01027c82665bd0d56e0ba46903ebe83d49a3
Pavel Novikov <mail@pavel.dev>
2023-05-08 19:32:39 +0300

    Add simple iframe with google page

--- client/web/src/jsMain/kotlin/dev.pavel.dashboard.client/WebClient.kt
+++ client/web/src/jsMain/kotlin/dev.pavel.dashboard.client/WebClient.kt
@@ -1,11 +1,22 @@
 package dev.pavel.dashboard.client
 
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.position
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Iframe
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        Text("Hello from WEB")
-        println("RENDER!!!")
+        Iframe(attrs = {
+            attr("src", "https://google.com")
+            style {
+                width(100.percent)
+                height(100.percent)
+                position(Position.Fixed)
+            }
+        })
     }
 }
