25511f4a669d2c61cc595e1488a19a1a081b0845
Pavel Novikov <mail@pavel.dev>
2023-09-23 21:12:51 +0300

    Add licence info

--- client/admin/src/commonMain/kotlin/dev/pavel/dashboard/admin/dashboards/DashboardPMDelegate.kt
+++ client/admin/src/commonMain/kotlin/dev/pavel/dashboard/admin/dashboards/DashboardPMDelegate.kt
@@ -1,3 +1,8 @@
+/*
+ * # Copyright: (c) 2023, Pavel Novikov <mail@pavel.dev>
+ * # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
 package dev.pavel.dashboard.admin.dashboards
 
 import dev.pavel.dashboard.admin.EditableCollectionChildPM
